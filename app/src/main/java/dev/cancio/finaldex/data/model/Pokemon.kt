271526406072065224a25d21b90e9c881b5a866b
Filipe Câncio <filipe.cancio@gmail.com>
2022-07-20 03:34:42 -0300

    atualizando repository

--- app/src/main/java/dev/cancio/finaldex/data/model/Pokemon.kt
+++ app/src/main/java/dev/cancio/finaldex/data/model/Pokemon.kt
@@ -1,6 +1,7 @@
 package dev.cancio.finaldex.data.model
 
 data class Pokemon(
+    val id: Int = 0,
     val name: String = "who that's pokemon",
     val avatarUrl: String = "https://i.pinimg.com/originals/95/d5/cd/95d5cded00f3a3e8a98fb1eed568aa9f.png",
 )
